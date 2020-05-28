4ede83b4101ba4f225a2f930b95ededc4c9e9c7c
IbnuAhsani <ibnu.muhari@gmail.com>
2020-05-28 13:07:44 +0700

    feat: functioning abstract prediction

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456789'
 
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('./classifiers/model_sastrawi.pkl', 'rb'))
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -16,9 +16,9 @@ def home():
 
   if form.validate_on_submit():
     abstract = form.abstract.data
-    prediction = predict(abstract)
+    prediction = predict(abstract, model)
 
-    return '<h1> the username is {}.'. format(prediction)
+    return '<h1> the the class is {}.'. format(prediction)
 
   return render_template("home.html", title='Form Submition', form=form)
 
