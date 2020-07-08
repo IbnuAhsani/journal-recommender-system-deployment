fa650360ba16a3bdc9bdaaf10423d73f53b584d8
IbnuAhsani <ibnu.muhari@gmail.com>
2020-07-08 20:52:12 +0700

    fix: impl manual sw removal using Tala (2003) sw list

--- utils/text_processing.py
+++ utils/text_processing.py
@@ -5,7 +5,6 @@ import nltk
 import string
 import numpy as np
 from collections import OrderedDict 
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 
@@ -17,18 +16,23 @@ def test_append(text):
 
 def preprocess(abstract):
 
-    stop_word_factory = StopWordRemoverFactory()
+    with open('./data/stop-words.json') as f:
+        stop_words = json.load(f)
+
     stemmer_factory = StemmerFactory()
-    stopword = stop_word_factory.create_stop_word_remover()
     stemmer = stemmer_factory.create_stemmer()
+
+    remove_digits = str.maketrans(string.digits, ' '*len(string.digits))
+    remove_punctuations = str.maketrans(string.punctuation, ' '*len(string.punctuation))
 
     abstract_lower_case = abstract.lower()
     abstract_white_spaces_removed = abstract_lower_case.strip()
-    abstract_numbers_removed = abstract_white_spaces_removed.translate(string.digits)
-    abstract_punctuation_removed = abstract_numbers_removed.translate(string.punctuation)
-    abstract_stopword_removed = stopword.remove(
-        abstract_punctuation_removed)
-    abstract_stemmed = stemmer.stem(abstract_stopword_removed)
+    abstract_numbers_removed =  abstract_white_spaces_removed.translate(remove_digits)
+    abstract_punctuation_removed = abstract_numbers_removed.translate(remove_punctuations)
+    abstract_punctuation_removed_list = abstract_punctuation_removed.split()
+    abstract_sw_removed_list = [word for word in abstract_punctuation_removed_list if word not in stop_words]
+    abstract_sw_removed = " ".join(abstract_sw_removed_list)    
+    abstract_stemmed = stemmer.stem(abstract_sw_removed)
     abstract_tokens = nltk.word_tokenize(abstract_stemmed)
 
     final_token = []
